f478054566e4c609455abc35563855a0c9a95789
lavendis <wongsatorn.saehao@gmail.com>
2025-10-06 16:02:51 +0700

    Final Laewwwwgit add .git add .git add .

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-const my_input = document.getElementById("my_input")
+// const my_input = document.getElementById("my_input")
 const accountbal = document.getElementById("accountbal")
 const cashbal = document.getElementById("cashbal")
+const logs = document.getElementById("result")
+const opAmount = document.getElementById("bankOp")
 
 function add() {
     my_input.value = Number(my_input.value) + 1
 }
 
-function setinput() {
-    accountbal.value =
-    cashbal.value =
+function setBalance() {
+    account = Number(accountbal.value) || 0
+    cash = Number(cashbal.value) || 0
+    
+    logs.value += `Current account balance: ${account}, Current cash balance: ${cash}\n`
+}
+
+function bankOp() {
+    amount = Number(opAmount.value) || 0
+    const type = document.getElementById("dropdown")
+    const selected = type.options[type.selectedIndex].text.toLowerCase()
+    
+    if (amount >= 1) {
+        if (selected == "deposit" && amount <= cash) {
+            cash -= amount
+            account += amount
+            logs.value += `Current account balance: ${account}, Current cash balance: ${cash}\n`
+        } else if (selected == "withdraw" && amount <= account) {
+            cash += amount
+            account -= amount
+            logs.value += `Current account balance: ${account}, Current cash balance: ${cash}\n`
+        } else {
+            logs.value += `Fail!!!\n`
+        }
+    }
 }

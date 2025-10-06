aa5014c5af4f34a27bde7713060796851cc5eb42
lavendis <wongsatorn.saehao@gmail.com>
2025-10-06 15:20:01 +0700

    draft before JavaScript

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const my_input = document.getElementById("my_input")
+const accountbal = document.getElementById("accountbal")
+const cashbal = document.getElementById("cashbal")
 
 function add() {
     my_input.value = Number(my_input.value) + 1
+}
+
+function setinput() {
+    accountbal.value =
+    cashbal.value =
 }
